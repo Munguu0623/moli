9c1599cbecbd6f7031483ba3e0ad223173d12cf5
M0N60N <macintosh@Macintoshs-MacBook-Pro.local>
2023-03-28 22:56:53 +0800

    loeader add

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
+import 'nprogress/nprogress.css'
 import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
@@ -11,9 +11,21 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-
+import Router, { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+
+Router.onRouteChangeStart = (url) => {
+  console.log(url)
+  NProgress.start()
+}
+Router.onRouteChangeComplete = (url) => {
+  NProgress.done()
+}
+Router.onRouteChangeError = (url) => {
+  NProgress.done()
+}
 
 export default function App({ Component, pageProps }) {
   return (
@@ -26,6 +38,7 @@ export default function App({ Component, pageProps }) {
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
+      {/* <NextNProgress options={{ showSpinner: false }} /> */}
     </ThemeProvider>
   )
 }
