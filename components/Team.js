2a58906c6b07ad1f2c053f72008ea04f4cfe9362
M0N60N <macintosh@Macintoshs-MacBook-Pro.local>
2023-04-09 17:48:31 +0800

    team zaswar

--- components/Team.js
+++ components/Team.js
@@ -41,7 +41,7 @@ export default function Team(second) {
               </div>
               <div className="space-y-4 text-center">
                 <div>
-                  <h4 className="text-2xl text-gray-700 dark:text-white">Буяндэдгэр</h4>
+                  <h4 className="text-2xl text-gray-700 dark:text-white">Буяндэлгэр</h4>
                   <span className="block text-sm text-gray-500">Project manager</span>
                 </div>
               </div>
